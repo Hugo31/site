0ae78b1062313ffb0266c8dd05aaf2f65907ca02
loicviguier <loicviguier@localhost>
2014-01-20 11:19:17 +0100

    Merge origin/master
    Conflicts:
    	TODO.txt

--- test/conflictsolution/ajouterSolution.php
+++ test/conflictsolution/ajouterSolution.php
@@ -1,8 +1,8 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+    header('Location: ../pagetestsolution.php');
+    require_once($_SERVER['DOCUMENT_ROOT']."site/model/conflictSolution/Solution.php");
+    if(isset($_POST['comment']) and isset($_POST['code']) and isset($_POST['user'])){
+        $sol = new Solution(0, $_POST['comment'], $_POST['code'], NOW(), 0, $_POST['user']);
+        Solution::addDB($sol);
+    }
+?>
