185cbcfec7b79d70986378bb6d7e1d6e1ee20900
loicviguier <loicviguier@gmail.com>
2014-03-11 14:57:10 +0100

    Merge origin/master 	-> Signup Ok 	-> Forget Ok 	Changement de place du dossier inscription dans Source Files

--- inscription/ctrlSignup.js
+++ inscription/ctrlSignup.js
@@ -9,8 +9,6 @@ var formulaire;
 function validSignup(form) {
 
     formulaire = form;
-    
-    alert('pop');
 
     var veriff = verifFirstname(form.find("#firstnamesignup"));
     var verifl = verifLastname(form.find("#lastnamesignup"));
@@ -29,10 +27,17 @@ function validSignup(form) {
 function verifFirstname(champ) {
     var champErreur = document.getElementById("errormsgsignup_firstname");
     var firstname = champ.prop("value");
-    var regex = new RegExp('[A-z]{3,30}');
-    
-    alert('popf');
-    
+    var regex = new RegExp('^([A-z]+([ |\-]{1}([A-z])+)*){2,30}$');
+
+    // Verification de longueur
+    if (firstname.length < 2 || firstname.length > 30) {
+        champ.focus();
+        champ.css("backgroundColor", "#FBA");
+        champErreur.style.color = '#FBA';
+        champErreur.innerHTML = 'Your firstname is too short or too long</br>';
+        return false;
+    }
+
     if (regex.test(firstname)) {
         champ.css("backgroundColor", "#FFF");
         champErreur.innerHTML = '';
@@ -49,10 +54,17 @@ function verifFirstname(champ) {
 function verifLastname(champ) {
     var champErreur = document.getElementById("errormsgsignup_lastname");
     var lastname = champ.prop("value");
-    var regex = new RegExp('[A-z]{3,30}');
-    
-    alert('popl');
-    
+    var regex = new RegExp('^([A-z]+([ |\-]{1}([A-z])+)*){2,30}$');
+
+    // Verification de longueur
+    if (lastname.length < 2 || lastname.length > 30) {
+        champ.focus();
+        champ.css("backgroundColor", "#FBA");
+        champErreur.style.color = '#FBA';
+        champErreur.innerHTML = 'Your lastname is too short or too long</br>';
+        return false;
+    }
+
     if (regex.test(lastname)) {
         champ.css("backgroundColor", "#FFF");
         champErreur.innerHTML = '';
@@ -69,35 +81,41 @@ function verifLastname(champ) {
 function verifUsername(champ) {
     var username = champ.prop("value");
     var champErreur = document.getElementById("errormsgsignup_username");
-    
-    alert('popu');
+    var regex = new RegExp('^[A-z0-9]{3,30}$');
+
+    // Verification de longueur
+    if (username.length < 3 || username.length > 30) {
+        champ.focus();
+        champ.css("backgroundColor", "#FBA");
+        champErreur.style.color = '#FBA';
+        champErreur.innerHTML = 'Your username is too short or too long</br>';
+        return false;
+    }
 
     // Verification syntaxique
-    if (username.length < 4 || username.length > 30) {
-        champ.css("backgroundColor", "#FBA");
-        champ.focus();
-        champErreur.style.color = '#FBA';
-        champErreur.innerHTML = 'Your username is too short or too longer</br>';
-        return false;
-    } else {
+    if (regex.test(username)) {
         // Verification d'unicité de l'username dans la BD
-        if ($.post('ctrlSignupUsername.php', {username: username}, function(data) {
+        return ($.post('ctrlSignupUsername.php', {username: username}, function(data) {
             var champUsername = formulaire.find('#usernamesignup');
             var champErreur = document.getElementById("errormsgsignup_username");
-            if (data[0] == '1') {
+            if (data[0] == '0') {
+                champ.css("backgroundColor", "#FFF");
+                champErreur.innerHTML = '';
+                return true;
+            } else {
                 champUsername.focus();
                 champUsername.css("backgroundColor", "#FBA");
                 champErreur.style.color = '#FBA';
                 champErreur.innerHTML = 'Your username is already exist</br>';
-                return true;
-            } else {
-                champ.css("backgroundColor", "#FFF");
-                champErreur.innerHTML = '';
                 return false;
             }
-        }))
-            return false;
-        return true;
+        }));
+    } else {
+        champ.focus();
+        champ.css("backgroundColor", "#FBA");
+        champErreur.style.color = '#FBA';
+        champErreur.innerHTML = 'Your username must not contain special characters</br>';
+        return false;
     }
 }
 
@@ -105,60 +123,71 @@ function verifEmail(champ) {
     var email = champ.prop("value");
     var champErreur = document.getElementById("errormsgsignup_email");
     var regex = new RegExp('^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$', 'i');
-    
-    alert('pope');
-    
+
     // Vérification syntaxique
-    if (!regex.test(email)) {
+    if (regex.test(email)) {
+        // Verification d'unicité de l'email dans la BD
+        return ($.post('ctrlSignupEmail.php', {email: email}, function(data) {
+            var champEmail = formulaire.find('#emailsignup');
+            var champErreur = document.getElementById("errormsgsignup_email");
+            if (data[0] == '0') {
+                champ.css("backgroundColor", "#FFF");
+                champErreur.innerHTML = '';
+                return true;
+            } else {
+                champEmail.focus();
+                champEmail.css("backgroundColor", "#FBA");
+                champErreur.style.color = '#FBA';
+                champErreur.innerHTML = 'This email is already exist</br>';
+                return false;
+            }
+        }));
+    } else {
         champ.focus();
         champ.css("backgroundColor", "#FBA");
         champErreur.style.color = '#FBA';
         champErreur.innerHTML = 'please, put a valid email address</br>';
         return false;
-    } else {
-        // Verification d'unicité de l'email dans la BD
-        if ($.post('ctrlSignupEmail.php', {email: email}, function(data) {
-            var champEmail = formulaire.find('#emailsignup');
-            var champErreur = document.getElementById("errormsgsignup_email");
-            if (data[0] == '1') {
-                champEmail.focus();
-                champEmail.css("backgroundColor", "#FBA");
-                champErreur.style.color = '#FBA';
-                champErreur.innerHTML = 'This email is already exist</br>';
-                return true;
-            } else {
-                champ.css("backgroundColor", "#FFF");
-                champErreur.innerHTML = '';
-                return false;
-            }
-        }))
-            return false;
-        return true;
     }
 }
 
 function verifSimilarity(champ1, champ2) {
+    var pass1 = champ1.prop("value");
+    var pass2 = champ2.prop("value");
     var champErreur = document.getElementById("errormsgsignup_password");
-    
-    alert('popp');
-    
-    if (champ1.prop("value") == '' || champ2.prop("value") == '') {
-        champ1.css("backgroundColor", "#FBA");
-        champ2.css("backgroundColor", "#FBA");
-        champErreur.style.color = '#FBA';
-        champErreur.innerHTML = 'Please, give a password</br>';
-        champ1.focus();
-        return false;
-    } else if (champ1.prop("value") == champ2.prop("value")) {
-        champ1.css("backgroundColor", "#FFF");
-        champ2.css("backgroundColor", "#FFF");
-        return true;
+    var regex = new RegExp('^[A-z0-9]{4,30}$');
+
+    if (regex.test(pass1)) {
+        if (pass1 == pass2) {
+            champ1.css("backgroundColor", "#FFF");
+            champ2.css("backgroundColor", "#FFF");
+            champErreur.innerHTML = '';
+            return true;
+        }
+
+        if (pass1 != pass2) {
+            champ2.focus();
+            champ1.css("backgroundColor", "#FBA");
+            champ2.css("backgroundColor", "#FBA");
+            champErreur.style.color = '#FBA';
+            champErreur.innerHTML = 'Please, give the same password</br>';
+            champ1.focus();
+            return false;
+        }
+
+        if (pass1 == '' && pass2 == '') {
+            champ1.css("backgroundColor", "#FBA");
+            champ2.css("backgroundColor", "#FBA");
+            champErreur.style.color = '#FBA';
+            champErreur.innerHTML = 'Please, give a password</br>';
+            champ1.focus();
+            return false;
+        }
     } else {
         champ1.css("backgroundColor", "#FBA");
         champ2.css("backgroundColor", "#FBA");
         champErreur.style.color = '#FBA';
-        champErreur.innerHTML = 'Please, give the same password</br>';
-        champ1.focus();
+        champErreur.innerHTML = 'Password must not contain special characters</br>';
         return false;
     }
 }
--- inscription/validsignup.php
+++ inscription/validsignup.php
@@ -1,13 +1,15 @@
 <?php
 
-    header('Location: /site/indexlaurine/index.html');
+header('Location: /site/indexlaurine/index.html');
 
-    require_once($_SERVER['DOCUMENT_ROOT'] . "/site/model/Database.php");
-    require_once($_SERVER['DOCUMENT_ROOT'] . "/site/model/user/User.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/site/model/Database.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/site/model/user/User.php");
 
-    if (isset($_POST['usernamesignup']) AND isset($_POST['passwordsignup']) AND isset($_POST['emailsignup'])) {
-        $user = new User($_POST['usernamesignup'], $_POST['passwordsignup'], '', '', $_POST['emailsignup'], '');
-        User::addDB($user);
-    }
-
-?>
+if (isset($_POST['usernamesignup']) AND isset($_POST['lastnamesignup']) 
+        AND isset($_POST['firstnamesignup']) AND isset($_POST['passwordsignup']) 
+        AND isset($_POST['emailsignup'])) {
+    $user = new User($_POST['usernamesignup'], $_POST['passwordsignup'], 
+            $_POST['lastnamesignup'], $_POST['firstnamesignup'], 
+            $_POST['emailsignup'], NULL);
+    User::addDB($user);
+}
