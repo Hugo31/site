5a04d58e907dfad7896371bf3aa687bd73f7fdad
loicviguier <loicviguier@gmail.com>
2014-03-24 09:15:12 +0100

    Merge origin/master
    Conflicts:
    	BDProject.mwb
    	BDProject.mwb.bak
    	controller/addCart.php
    	controller/addRate.php
    	controller/removeCart.php
    	controller/sign/validsignup.php
    	controller/toolkit/ToolKitDisplay.php
    	javascript/toolkit.js
    	model/interfaces/ETypeUser.php
    	patternInterface.sql
    	todo.xlsx
    	view/contact.php
    	view/forget.php
    	view/signup.php
    	view/structure/header.php

--- controller/sign/logout.php
+++ controller/sign/logout.php
@@ -2,5 +2,6 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . "/site/controller/toolkit/Session.php");
 $session = Session::getInstance();
 unset($session->login);
+unset($session->admin);
 
 header('Location: /site/index.php');
--- view/structure/header.php
+++ view/structure/header.php
@@ -1,8 +1,8 @@
 <?php
-    $session = Session::getInstance();
-    require_once($_SERVER['DOCUMENT_ROOT']."/site/model/implementation/Database.php");
-    require_once($_SERVER['DOCUMENT_ROOT']."/site/controller/toolkit/ToolKitDisplay.php");
-    require_once($_SERVER['DOCUMENT_ROOT']."/site/controller/toolkit/ToolkitSearch.php");
+$session = Session::getInstance();
+require_once($_SERVER['DOCUMENT_ROOT'] . "/site/model/implementation/Database.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/site/controller/toolkit/ToolKitDisplay.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/site/controller/toolkit/ToolkitSearch.php");
 ?>
 
 <!DOCTYPE html>
@@ -25,7 +25,7 @@
         <link rel="stylesheet" type="text/css" href="/site/styles/stylePlus.css" media="screen" />
         <link rel="stylesheet" type="text/css" href="/site/styles/styleRate.css" media="screen" />
         <link rel="stylesheet" type="text/css" href="/site/styles/styleAdd.css" media="screen" />
-        
+
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.12/jquery-ui.min.js"></script>
         <script type="text/javascript" src="/site/javascript/toolkit.js"></script>
@@ -39,50 +39,56 @@
             <header id="header">
                 <div id="headercontent">
                     <a href="/site/index.php"><img src="/site/img/header/logo_modif_gris.png" name="image" style="height:80px"
-                       onmouseover="image.src='/site/img/header/logo_modif_gris_souris.png'" onmouseout="image.src='/site/img/header/logo_modif_gris.png'"></a>
+                                                   onmouseover="image.src = '/site/img/header/logo_modif_gris_souris.png'" onmouseout="image.src = '/site/img/header/logo_modif_gris.png'"></a>
                     <div id="navigation">
-                        <?php if (isset($session->login) || isset($session->admin)) {
-                            echo "<div id=\"connecte\">Hi ". $session->login."!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-                                . "<a href=\"/site/view/profil.php\">See my profile</a>"
-                                . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href=\"/site/controller/deconnexion.php\" style=\"text-decoration:none\"><input value=\"LOG OUT\" type=\"button\" class=\"logout\" ></a></div>";                       
-                        } else { ?>
-                            <div style="height:50px">
-                            <form method="post" id="loginForm2" name="loginform2"
-                                  action="/site/controller/sign/validSignin.php" onsubmit="return validSignin($(this));">
-                                
-                                <input type="text" id="loginsignin" name="loginsignin" 
-                                       required="required" placeholder="Username or Email">
-                                <input type="password" id="passwordsignin" name="passwordsignin"
-                                       required="required" placeholder="Password">
-                                <input value="SIGN IN" type="submit" class="signin" >
-                                
-                                <a href="/site/view/signup.php" style="text-decoration: none" >
-                                    <input value="SIGN UP" type="button" class="signup">
+                        <?php if (isset($session->login) || isset($session->admin)) { ?>
+                            <div id="headerlogout" style="height: 50px">
+                                <img src = "/site/img/vrac/defaultlogo.png" alt = "image profil" style = "height: 35px; width: 35px">
+                                <a href = "/site/view/profil.php" style = "text-decoration: none"><?php echo $session->login; echo $session->admin ?></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <a href="" style="text-decoration: none">See my projects</a>&nbsp;&nbsp;&nbsp;&nbsp;
+                                <a href="/site/controller/sign/logout.php" style="text-decoration:none">
+                                    <button>LOG OUT</button>
                                 </a>
-                                
-                            </form>
-                            <div id="errorlogin" style="float: left; margin-left: 75px; margin-top: 3px"></div>
-                            <div id="forgottenPassword"><a href="/site/view/forget.php">> Forgotten password?</a></div>
-                        </div>
-                        <?php } 
+                            </div>
+                        <?php } else { ?>
+                            <div style="height:50px">
+                                <form method="post" id="loginForm2" name="loginform2"
+                                      action="/site/controller/sign/validSignin.php" onsubmit="return validSignin($(this));">
+
+                                    <input type="text" id="loginsignin" name="loginsignin" 
+                                           required="required" placeholder="Username or Email">
+                                    <input type="password" id="passwordsignin" name="passwordsignin"
+                                           required="required" placeholder="Password">
+                                    <input value="SIGN IN" type="submit" class="signin" >
+
+                                    <a href="/site/view/signup.php" style="text-decoration: none" >
+                                        <input value="SIGN UP" type="button" class="signup">
+                                    </a>
+
+                                </form>
+                                <div id="errorlogin" style="float: left; margin-left: 75px; margin-top: 3px"></div>
+                                <div id="forgottenPassword"><a href="/site/view/forget.php">> Forgotten password?</a></div>
+                            </div>
+                            <?php
+                        }
                         echo '<nav id="menu">';
                         if (isset($session->admin)) {
-                        ?>
+                            ?>
                             <a href="/site/index.php">Home</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                             <a href="">Users</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                             <a href="">Design Patterns</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                             <a href="">Conflicts</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                             <a href="">Solutions</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                             <a href="">Projects</a>
-                        <?php
-                            } else {
-                        ?>                   
+                            <?php
+                        } else {
+                            ?>                   
                             <a href="/site/index.php">Home</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                             <a href="/site/view/news.php">News</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                             <a href="/site/view/currentDP.php">My current Design Pattern (<span class="currentDP_numberIn"><?php ToolKitSearch::getNbCurrentCart($session) ?></span>)</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                             <a href="/site/view/contact.php">Contact</a>
+<?php } ?>
                         </nav>
-                        <?php } ?>
                     </div>
                 </div>
             </header>
