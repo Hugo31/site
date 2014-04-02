48ffb7208acf0adc963286f2c3dd33d47a2435c5
loicviguier <loicviguier@gmail.com>
2014-04-03 01:06:43 +0200

    footer description

--- view/about.php
+++ view/about.php
@@ -43,7 +43,14 @@
     <br/><br/>
     <div id="zonegrise">
         <img src="/site/img/vrac/guillemet2.png" width="30px" style="padding-right:5px;">
-        MF3
+        La marche des vertueux est semée d'obstacles qui sont les entreprises égoïstes 
+        que fait sans fin surgir l'œuvre du Malin. Béni soit-il l'homme de bonne volonté 
+        qui, au nom de la charité, se fait le berger des faibles qu'il guide dans la vallée 
+        d'ombre de la mort et des larmes... Car il est le gardien de son frère et la providence 
+        des enfants égarés. J'abattrai alors le bras d'une terrible colère, d'une vengeance furieuse 
+        et effrayante sur les hordes impies qui pourchassent et réduisent à néant les brebis de 
+        Dieu. Et tu connaîtras pourquoi mon nom est l'Éternel quand sur toi s'abattra la vengeance 
+        du Tout-Puissant !
     </div>
 </section>
 
