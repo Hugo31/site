1b6dc975a19c0c0b909da62e3e93cf49ccad82cc
Loic <loicfaure@hotmail.fr>
2014-04-10 16:10:58 +0200

    Desktop version

--- view/about.php
+++ view/about.php
@@ -24,32 +24,22 @@
     <h3>> Hugo GUIGNARD</h3>
     <div id="zonegrise">
         <img src="/site/img/vrac/guillemet2.png" width="30px" style="padding-right:5px;">
-        Legends say that Hugo was raised by wolves in the mystical mountains of Saint-Clar. Model of wisdom, 
-        goodness and awesomeness, Hugo is considered by his peers as a superior spirit who appeared to 
-        help the human race to overcome its challenges.
+        Senor guignaro de la hoya !
     </div>
     <h3>> Laurine MARMISSE</h3>
     // faire sa présentation
     <br/><br/>
     <div id="zonegrise">
         <img src="/site/img/vrac/guillemet2.png" width="30px" style="padding-right:5px;">
-        mon plat stp<br>
-        ofet, what else?<br>
-        Laurine c'est la plus gentille des membres de l'équipe, en plus elle est juste trop sex :P
+        Pur énergie de swag
     </div>
     <h3>> Loïc VIGUIER</h3>
     // faire sa présentation
     <br/><br/>
     <div id="zonegrise">
         <img src="/site/img/vrac/guillemet2.png" width="30px" style="padding-right:5px;">
-        La marche des vertueux est semée d'obstacles qui sont les entreprises égoïstes 
-        que fait sans fin surgir l'œuvre du Malin. Béni soit-il l'homme de bonne volonté 
-        qui, au nom de la charité, se fait le berger des faibles qu'il guide dans la vallée 
-        d'ombre de la mort et des larmes... Car il est le gardien de son frère et la providence 
-        des enfants égarés. J'abattrai alors le bras d'une terrible colère, d'une vengeance furieuse 
-        et effrayante sur les hordes impies qui pourchassent et réduisent à néant les brebis de 
-        Dieu. Et tu connaîtras pourquoi mon nom est l'Éternel quand sur toi s'abattra la vengeance 
-        du Tout-Puissant !
+        La bible toussa toussa
+        Catholicisme, islamomusulmanoterroriste
     </div>
 </section>
 
