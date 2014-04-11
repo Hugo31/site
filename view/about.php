2db6d8562a0ec36249f40b0523825a6daedf4d65
loicviguier <loicviguier@gmail.com>
2014-04-11 11:01:02 +0200

    about.php final

--- view/about.php
+++ view/about.php
@@ -15,37 +15,37 @@
     development and integration to create the site brought to you today.
     <h2>Our staff</h2>
     <h3>> Loïc FAURE</h3>
-    <br/>
     <div id="zonegrise">
         <img src="/site/img/vrac/guillemet2.png" width="30px" style="padding-right:5px;">      
         Loïc is from Ariege, he graduated from Blagnac's IUT and the IT license of Paul Sabatier.
         He has worked professionally in the development of applications for UAVs.
         Loïc likes food, skateboarding and to work out.
     </div>
-    <h3>> Hugo GUIGNARD</h3>
     <br/>
+    <h3>> Hugo GUIGNARD</h3>
     <div id="zonegrise">
         <img src="/site/img/vrac/guillemet2.png" width="30px" style="padding-right:5px;">
         Hugo is from Toulouse, he graduated from Blagnac's IUT and the IT license of Paul Sabatier.
         He has worked professionally in the development of applications for UAVs.
         Hugo likes to work out, play tennis and to get cultural enrichment.
     </div>
-    <h3>> Laurine MARMISSE</h3>
     <br/>
+    <h3>> Laurine MARMISSE</h3>
     <div id="zonegrise">
         <img src="/site/img/vrac/guillemet2.png" width="30px" style="padding-right:5px;">
         Laurine is from Figeac, she graduated from Rodez's IUT and the IT license of Paul Sabatier.
         She has experience in web development and Database. She's specialized in HCI.
         Laurine likes movies, photography and gymnastic.
     </div>
-    <h3>> Loïc VIGUIER</h3>
     <br/>
+    <h3>> Loïc VIGUIER</h3>
     <div id="zonegrise">
         <img src="/site/img/vrac/guillemet2.png" width="30px" style="padding-right:5px;">
         Loïc is from Labège, he graduated from the IT license of Paul Sabatier.
         He has experience in iOS and web development.
         Loïc likes sport, series and play guitare since 2005.
     </div>
+    <br/>
 </section>
 
 <?php
