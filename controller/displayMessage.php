b090640d0748a7a45bdf94a46049b46b646387df
Loic <loicfaure@hotmail.fr>
2014-04-28 01:33:38 +0200

    Preparation serveur

--- controller/displayMessage.php
+++ controller/displayMessage.php
@@ -1,23 +1,12 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT']."/site/controller/toolkit/Session.php");
-$session = Session::getInstance();
 
-if (!isset($session->message)) {
-    $session->message = array();
-}
 if (isset($_GET['typeMessage'])) {
-    $message = $session->message;
-    $message[count($message)] = $_GET['typeMessage'];
+    echo "<div class=\"message\">";
+    //Faire un système de type.
+    echo $_GET['typeMessage'];
+    echo "</div>";
 }
 
 ?>
-<div class="message-container">
-    <?php
-        foreach ($session->message as $m) {
-            echo "<div class=\"message\">";
-        
-            echo "</div>";
-        }
-    ?>
-</div>
+
 
