fc1b822f603df6945d7c8e7b74010cfb322ce24b
Loic <loicfaure@hotmail.fr>
2014-05-09 12:31:35 +0200

    Final modif

--- testJQuery/testpage.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once($_SERVER['DOCUMENT_ROOT']."/site/model/implementation/criteria/Category.php");
-$cat = new Category(0, $_POST['nom'], $_POST['description']);
-echo Category::addDB($cat);
-
-?>
